8ee613efd8282c8da676bbdeae16380f86f178d5
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-16 11:36:00 +0500

    Update componenets

--- src/componenets/Filter/Filter.js
+++ src/componenets/Filter/Filter.js
@@ -1,5 +1,18 @@
+import './Filter.css'
 const Filter = () => {
-	return <div>Filters</div>
+	return (
+		<div className='btns-group'>
+			<button className='btn btn-dark' type='button'>
+				All movies
+			</button>
+			<button className='btn btn-outline-dark' type='button'>
+				Popular movies
+			</button>
+			<button className='btn btn-outline-dark' type='button'>
+				Most watched movies
+			</button>
+		</div>
+	)
 }
 
 export default Filter
--- src/componenets/Search-Panel/SearchPanel.js
+++ src/componenets/Search-Panel/SearchPanel.js
@@ -1,5 +1,13 @@
+import './SearchPanel.css'
+
 const SeachPanel = () => {
-	return 'SeachPanel'
+	return (
+		<input
+			type='text'
+			className='form-control search-input'
+			placeholder='Search Movies'
+		/>
+	)
 }
 
 export default SeachPanel
