2dd60e50c01650bc315294704916a5dfbcacace5
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-18 09:49:09 +0500

    Update

--- src/componenets/MovieList/MovieList.js
+++ src/componenets/MovieList/MovieList.js
@@ -6,9 +6,9 @@ const MovieList = () => {
 	return (
 		<div className='movie-list'>
 			Movie List:
-			<MovieListItem />
-			<MovieListItem />
-			<MovieListItem />
+			<MovieListItem name={'Home Alone'} viewers={576} />
+			<MovieListItem name={'Home Alone 2'} viewers={235} />
+			<MovieListItem name={'Spider Man'} viewers={450} />
 		</div>
 	)
 }
--- src/componenets/MovieListItem/MovieListItem.js
+++ src/componenets/MovieListItem/MovieListItem.js
@@ -1,10 +1,14 @@
 import './MovieListItem.css'
 
-const MovieListItem = () => {
+const MovieListItem = ({ name, viewers }) => {
 	return (
 		<li className='list-group-item d-flex justify-content-between'>
-			<span className='list-group-item-label'>Home Alone</span>
-			<input type='number' className='list-group-item-input' defaultValue='1' />
+			<span className='list-group-item-label'>{name}</span>
+			<input
+				type='number'
+				className='list-group-item-input'
+				defaultValue={viewers}
+			/>
 			<div className='d-flex justify-content-center align-items-center'>
 				<button type='button' className='btn-cookie btn-sm'>
 					<i className='fas fa-cookie'></i>
